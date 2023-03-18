f74ef56605532bc5a835a167d89fbc5a1819bf0f
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-18 08:51:37 +0300

    tetsing-compose-pipe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { compose, pipe } from "lodash/fp";
 const App = () => {
-  return <h1>App</h1>;
+  const x = 2;
+  const double = (number) => number * 2;
+  const square = (number) => number * number;
+  const half = (number) => number / 2;
+  const mathCalculate = pipe(double, square, half);
+  return <h1>{mathCalculate(x)}</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
