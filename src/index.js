9b4a2cea70098e8c9f4e7e8e7f3cded13bf627aa
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-18 09:23:23 +0300

    muthadble-object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { compose, pipe } from "lodash/fp";
+
 const App = () => {
-  const x = 2;
-  const double = (number) => number * 2;
-  const square = (number) => number * number;
-  const half = (number) => number / 2;
-  const mathCalculate = pipe(double, square, half);
-  return <h1>{mathCalculate(x)}</h1>;
+  const [state, setState] = useState({});
+
+  const obj1 = { id: "2", name: "someName", author: { name: "some name" } };
+  const obj2 = { ...obj1, author: { ...obj1.author } };
+  console.log(obj1.author === obj2.author);
+  return <h1>App</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
