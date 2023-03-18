a065918253ee027c148168bf87a7c316c39d859b
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-18 11:11:46 +0300

    create-reducer

--- src/index.js
+++ src/index.js
@@ -1,41 +1,49 @@
+import { reduce } from "lodash";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function createStore(initialState) {
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "task/complited":
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].complited = true;
+      return newArray;
+
+    default:
+      break;
+  }
+}
+
+function createStore(reducer, initialState) {
   let state = initialState;
   function getState() {
     return state;
   }
   function dispatch(action) {
     if (action.type === "task/complited") {
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      );
-      newArray[elementIndex].complited = true;
-      state = newArray;
-      console.log(state);
+      state = reducer(state, action);
     }
   }
 
   return { getState, dispatch };
 }
-const store = createStore([
-  { id: 1, description: "Task 1", complited: "false" },
+const store = createStore(taskReducer, [
+  { id: 1, description: "Task 1", complited: false },
 ]);
 const App = () => {
   console.log(store.getState());
+  const complitedTask = () => {
+    store.dispatch({ type: "task/complited", payload: { id: 1 } });
+    console.log(store.getState());
+  };
 
   return (
     <>
       <h1>App</h1>
-      <button
-        onClick={() =>
-          store.dispatch({ type: "task/complited", payload: { id: 1 } })
-        }
-      >
-        Complete
-      </button>
+      <button onClick={complitedTask}>Complete</button>
     </>
   );
 };
