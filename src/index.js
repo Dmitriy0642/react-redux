1a868a26123268b57068e1c8afd7ea151975e19e
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-18 10:58:16 +0300

    create-store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-  const [state, setState] = useState({});
+function createStore(initialState) {
+  let state = initialState;
+  function getState() {
+    return state;
+  }
+  function dispatch(action) {
+    if (action.type === "task/complited") {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].complited = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
 
-  const obj1 = { id: "2", name: "someName", author: { name: "some name" } };
-  const obj2 = { ...obj1, author: { ...obj1.author } };
-  console.log(obj1.author === obj2.author);
-  return <h1>App</h1>;
+  return { getState, dispatch };
+}
+const store = createStore([
+  { id: 1, description: "Task 1", complited: "false" },
+]);
+const App = () => {
+  console.log(store.getState());
+
+  return (
+    <>
+      <h1>App</h1>
+      <button
+        onClick={() =>
+          store.dispatch({ type: "task/complited", payload: { id: 1 } })
+        }
+      >
+        Complete
+      </button>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
